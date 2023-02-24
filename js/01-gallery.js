1160dcc9bf99ff96d269c7b85ff550a21739067f
KarinaLiashko <112810602+KarinaLiashko@users.noreply.github.com>
2023-02-24 18:39:09 +0100

    Update-01-js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const imgCard = createImgCard(galleryItems);
+
+function createImgCard(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<div class="gallery__item"><a class="gallery__link"
+    href="${original}"><img class="gallery__image"
+    src="${preview}" data-source = "${original}"
+    alt = "${description}" width = "340"></img><a/></div>`
+    }).join("")
+};
+
+gallery.insertAdjacentHTML("afterbegin", imgCard);
+
+gallery.addEventListener('click', onImgOriginalCard);
+
+function onImgOriginalCard(e) {
+    e.preventDefault()
+    if (!e.target.dataset.source)
+        return console.log(e.target);
+}
+
+console.log(createImgCard);
